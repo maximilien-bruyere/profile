7c2f0030010d58885d30727865b30ce222d86e13
Bruyère Maximilien <maximilien-bruyere@hotmail.com>
2025-08-28 17:06:04 +0200

    chore: create Header, Footer, Home pages / modify App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css'
+import Footer from './components/layout/Footer'
+import Header from './components/layout/Header'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
+      <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
