cc4e23f227fafd900858e4f16f1dfdf5f69ec161
Bruyère Maximilien <maximilien-bruyere@hotmail.com>
2025-09-09 18:18:28 +0200

    remove background file and add .line-clamp style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import Background from "./components/layout/Background";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Profile from "./pages/profile/Profile";
@@ -7,7 +6,6 @@ import Profile from "./pages/profile/Profile";
 function App() {
   return (
     <div className="relative min-h-screen">
-      <Background />
 
       <div className="relative z-10 h-1000">
         <Header />
